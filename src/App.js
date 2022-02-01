7974f7283c36ad555e282ebd9b39ed7ac4a6d5b2
larkes-cyber <playegrovoy@gmail.com>
2022-02-01 20:25:42 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=(props)=>{
+  const [count,setCount]=useState(props.counter);
+  const setINC=(num)=>{
+    setCount(count=>count+num);
+  }
+  function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+  }
+  const setDEC=(num)=>{
+    setCount(count=>count-num);
+  }
+  const setRND=()=>{
+    setCount(getRandomInt(-50,50));
+  }
+  const reset=()=>{
+    setCount(0);
+  }
+    return (
+      <div class="app">
+        <div class="counter">{count}</div>
+        <div class="controls">
+          <button onClick={()=>setINC(1)}>INC</button>
+          <button onClick={()=>setDEC(1)}>DEC</button>
+          <button onClick={()=>setRND()}>RND</button>
+          <button onClick={()=>reset()}>RESET</button>
+        </div>
+      </div>
+    )
 }
 
+  
 export default App;
